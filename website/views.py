51993b1ef0320530812d11333a2e53d63ae34a3b
mihstr <micostrnad@gmail.com>
2023-06-22 16:24:10 +0200

    modified:   website/__pycache__/views.cpython-311.pyc 	modified:   website/forms.py 	modified:   website/templates/website/navbar.html 	modified:   website/templates/website/register.html 	modified:   website/views.py

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+from .forms import SignupForm
+
 # Create your views here.
 def home(request):
     
@@ -27,4 +29,27 @@ def logout_user(request):
     return redirect("website:home")
 
 def register_user(request):
-    return render(request, "website/register.html", {})
+
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You are registered!")
+            
+            return redirect("website:home")
+        
+        
+    else:
+        form = SignupForm()
+        return render(request, "website/register.html", {
+            "form": form,
+        })
+    
+    return render(request, "website/register.html", {
+        "form": form,
+    })
